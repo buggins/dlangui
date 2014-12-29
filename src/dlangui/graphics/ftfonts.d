d5dff418fe54201ab8247b08ab3f7b1095f3be02
Vadim Lopatin <buggins@fromru.com>
2014-12-29 08:50:00 +0300

    fix compilation with freetype

--- src/dlangui/graphics/ftfonts.d
+++ src/dlangui/graphics/ftfonts.d
@@ -34,10 +34,10 @@ private struct FontDef {
 		_italic = italic;
         _weight = weight;
 	}
-    const bool opEquals(ref const FontDef v) {
+    bool opEquals(ref const FontDef v) const {
         return _family == v._family && _italic == v._italic && _weight == v._weight && _face.equal(v._face);
     }
-    const hash_t toHash() const nothrow @safe {
+    hash_t toHash() const nothrow @safe {
         hash_t res = 123;
         res = res * 31 + cast(hash_t)_italic;
         res = res * 31 + cast(hash_t)_weight;
