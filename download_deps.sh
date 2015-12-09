9b73a5447cca707fd38c478d2e067da36a9db2b3
Vadim Lopatin <coolreader.org@gmail.com>
2015-12-09 19:29:41 +0300

    add deps scripts

---
+++ download_deps.sh
@@ -0,0 +1,14 @@
+cd dlangui
+mkdir deps
+cd deps
+git clone https://github.com/DerelictOrg/DerelictUtil.git
+git clone https://github.com/DerelictOrg/DerelictGL3.git
+git clone https://github.com/DerelictOrg/DerelictFT.git
+git clone https://github.com/DerelictOrg/DerelictSDL2.git
+git clone https://github.com/gecko0307/dlib.git
+git clone https://github.com/Dav1dde/gl3n.git
+git clone https://github.com/Devisualization/image.git de_image
+git clone https://github.com/Hackerpilot/libdparse.git
+git clone https://github.com/nomad-software/x11.git
+cd ../..
+cd dlangide || git clone https://github.com/buggins/dlangide.git
---
+++ update_deps.sh
@@ -0,0 +1,10 @@
+cd deps
+cd DerelictCocoa && git pull --tags origin master && cd ..
+cd DerelictGL3 && git pull --tags origin master && cd ..
+cd DerelictUtil && git pull --tags origin master && cd ..
+cd dlib && git pull --tags origin master && cd ..
+cd DerelictFT && git pull --tags origin master && cd ..
+cd DerelictSDL2 && git pull --tags origin master && cd ..
+cd de_image && git pull --tags origin master && cd ..
+cd gl3n && git pull --tags origin master && cd ..
+cd x11 && git pull --tags origin master && cd ..
