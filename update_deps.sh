ce4eea8f0cd4a0eef67a7147ef364f0aac117675
Vadim Lopatin <coolreader.org@gmail.com>
2016-01-14 13:28:50 +0300

    remove dependency clone/update scripts

--- update_deps.sh
+++
@@ -1,8 +0,0 @@
-cd deps
-cd DerelictCocoa && git pull --tags origin master && cd ..
-cd DerelictGL3 && git pull --tags origin master && cd ..
-cd DerelictUtil && git pull --tags origin master && cd ..
-cd DerelictFT && git pull --tags origin master && cd ..
-cd DerelictSDL2 && git pull --tags origin master && cd ..
-cd gl3n && git pull --tags origin master && cd ..
-cd x11 && git pull --tags origin master && cd ..
