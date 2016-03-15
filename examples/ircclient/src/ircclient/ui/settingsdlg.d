c9c3f82504128ca906cb5ec1c2f30add30d765a0
Vadim Lopatin <coolreader.org@gmail.com>
2016-03-15 16:57:50 +0300

    #201 improve settings dialog

--- examples/ircclient/src/ircclient/ui/settingsdlg.d
+++ examples/ircclient/src/ircclient/ui/settingsdlg.d
@@ -22,55 +22,34 @@ class SettingsDialog : Dialog {
         try {
             content = parseML(q{
                 VerticalLayout {
-                id: vlayout
-                        padding: Rect { 5, 5, 5, 5 }
-                layoutWidth: fill; layoutHeight: fill
-                        HorizontalLayout {
-                        layoutWidth: fill; layoutHeight: fill
-                                VerticalLayout {
-                                margins: 5
-                                        layoutWidth: 25%; layoutHeight: fill
-                                        TextWidget { text: "Project template" }
-                                    StringListWidget { 
-                                    id: projectTemplateList 
-                                            layoutWidth: wrap; layoutHeight: fill
-                                    }
-                                }
-                            VerticalLayout {
-                            margins: 5
-                                    layoutWidth: 40%; layoutHeight: fill
-                                    TextWidget { text: "Template description" }
-                                EditBox { 
-                                id: templateDescription; readOnly: true 
-                                    layoutWidth: fill; layoutHeight: fill
-                                }
-                            }
-                            VerticalLayout {
-                            layoutWidth: 35%; layoutHeight: fill
-                                    margins: 5
-                                    TextWidget { text: "Directory layout" }
-                                EditBox { 
-                                id: directoryLayout; readOnly: true
-                                    layoutWidth: fill; layoutHeight: fill
-                                }
-                            }
-                        }
+                    id: vlayout
+                    padding: Rect { 5, 5, 5, 5 }
+                    layoutWidth: fill; layoutHeight: fill
                     TableLayout {
-                    margins: 5
-                            colCount: 2
-                            layoutWidth: fill; layoutHeight: wrap
-                            TextWidget { text: "" }
-                        CheckBox { id: cbCreateWorkspace; text: "Create new solution"; checked: true }
-                        TextWidget { text: "Workspace name" }
-                        EditLine { id: edWorkspaceName; text: "newworkspace"; layoutWidth: fill }
-                        TextWidget { text: "" }
-                        CheckBox { id: cbCreateWorkspaceSubdir; text: "Create subdirectory for workspace"; checked: true }
-                        TextWidget { text: "Project name" }
-                        EditLine { id: edProjectName; text: "newproject"; layoutWidth: fill }
-                        TextWidget { text: "" }
-                        CheckBox { id: cbCreateSubdir; text: "Create subdirectory for project"; checked: true }
-                        TextWidget { text: "Location" }
-                        DirEditLine { id: edLocation; layoutWidth: fill }
+                        margins: 5
+                        colCount: 2
+                        layoutWidth: fill; layoutHeight: wrap
+
+                        TextWidget { text: "IRC Server host name" }
+                        EditLine { id: edHost; text: "irc.freenode.net"; layoutWidth: fill; minWidth: 400 }
+                        TextWidget { text: "IRC Server port" }
+                        EditLine { id: edHost; text: "6667"; layoutWidth: fill }
+                        TextWidget { text: " " }
+                        TextWidget { text: " " }
+                        TextWidget { text: "Nickname" }
+                        EditLine { id: edHost; text: "dlangui_test"; layoutWidth: fill }
+                        TextWidget { text: "Alternate nickname" }
+                        EditLine { id: edHost; text: "dlangui_tst2"; layoutWidth: fill }
+                        TextWidget { text: "Username" }
+                        EditLine { id: edHost; text: "user"; layoutWidth: fill }
+                        TextWidget { text: "Real name" }
+                        EditLine { id: edHost; text: "User Real Name"; layoutWidth: fill }
+                        TextWidget { text: " " }
+                        TextWidget { text: " " }
+                        TextWidget { text: "Channel to join on connect" }
+                        EditLine { id: edHost; text: "#d"; layoutWidth: fill }
+                        TextWidget { text: " " }
+                        CheckBox { id: cbCreateWorkspace; text: "Connect on startup"; checked: true }
                     }
                     TextWidget { id: statusText; text: ""; layoutWidth: fill }
                 }
