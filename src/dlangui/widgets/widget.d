b4330ab776c1c5a6c30c5a3b0ced977e0126552a
and3md <and3md@gmail.com>
2017-09-21 19:14:07 +0200

    Accelerator actions: update state before run and run only if enabled.

--- src/dlangui/widgets/widget.d
+++ src/dlangui/widgets/widget.d
@@ -1189,7 +1189,13 @@ public:
             Action action = findKeyAction(event.keyCode, event.flags); // & (KeyFlag.Shift | KeyFlag.Alt | KeyFlag.Control | KeyFlag.Menu)
             if (action !is null) {
                 //Log.d("Action found: ", action.id, " ", action.labelValue.id);
-                return dispatchAction(action);
+                // update action state
+                if (updateActionState(action, true) && action is _action)
+                    handleActionStateChanged();
+
+                //run only enabled actions
+                if (action.state.enabled)
+                    return dispatchAction(action);
             }
         }
         // handle focus navigation using keys
