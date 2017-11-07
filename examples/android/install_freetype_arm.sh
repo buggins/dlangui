cbd34f0be104d93f16f56b8cf863c22c0fbb3b5c
vennos5 <vennos@gmx.com>
2017-11-07 18:52:29 +0200

    Fix build paths

--- examples/android/install_freetype_arm.sh
+++ examples/android/install_freetype_arm.sh
@@ -8,13 +8,16 @@
 #Load settings for paths
 . ./android_build_config.mk
 
+inst=$(pwd)
 mkdir freetype
 cd freetype
 wget "https://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz"
 tar zxvf freetype-2.6.tar.gz
 cd freetype-2.6/
-./configure --host=arm-linux-androideabi --prefix="$(pwd)/output" --without-zlib --with-png=no --with-harfbuzz=no
+./configure --host=arm-linux-androideabi --without-zlib --prefix="$(pwd)/freetype" --with-png=no --with-harfbuzz=no
 make -j$(nproc)
 make install
-cp ./done/lib/libfreetype.so ../../libs/armeabi-v7a/libfreetype.so
+echo "ERNO ERNO ENOR"
+mkdir -p ../../libs/armeabi-v7a
+cp freetype/lib/libfreetype.so ../../libs/armeabi-v7a/libfreetype.so
 
