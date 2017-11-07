3f0fe0745e2fea3275d15f8b33e707a93eecc2a0
vennos5 <vennos@gmx.com>
2017-11-07 18:09:59 +0200

    Added freetype build script

---
+++ examples/android/install_freetype_arm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# You need working NDK standalone toolchain
+# $NDK/build/tools/make-standalone-toolchain.sh --platform=android-21 --install-dir=/some/dir --arch=arm
+#NDK_STANDALONE=/path/to/ndk-standalone-21-arm
+#PATH=$NDK_STANDALONE/bin:$PATH
+
+#Load settings for paths
+. ./android_build_config.mk
+
+mkdir freetype
+cd freetype
+wget "https://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.gz"
+tar zxvf freetype-2.6.tar.gz
+cd freetype-2.6/
+./configure --host=arm-linux-androideabi --prefix="$(pwd)/output" --without-zlib --with-png=no --with-harfbuzz=no
+make -j$(nproc)
+make install
+cp ./done/lib/libfreetype.so ../../libs/armeabi-v7a/libfreetype.so
+
